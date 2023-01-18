b43a8460c91b12bc09fd70749585d05d7085e058
Erick .oG Gutierrez <113930446+ErickG1234@users.noreply.github.com>
2023-01-18 18:58:02 +0000

    2 ideas

---
+++ Abstraction.py
@@ -0,0 +1,20 @@
+import math
+import random
+
+choice = input("Hello, where would you like to eat?")
+food_list = ["Chick-Fil-A", "Panera Bread", "Culvers", "McDonalds"]
+# idea 1
+#  Fast food options
+# Path - 1) Where would you like to go
+#  User picks a fast food place
+#  Here is what we reccomend from Place (*Shows 6 reccomended items)
+#  *User chooses items
+
+# idea 2
+# restuarant simulation
+# user chooses appetizer, dinner, dessert
+# user chooses a list and each list has 3 choices
+# The user picks an item and the cost and calories pops up
+# The program asks to stop or continue
+# The total calories and price will appear at the end
+# the program will ask the user if he wants to tip and how much percent
---
+++ main.py
@@ -0,0 +1,3 @@
+from abstraction import 
+# Ideas:
+# What should I eat today(Make a list with food and randomly chose a food from a healthy and unhealthy list)
